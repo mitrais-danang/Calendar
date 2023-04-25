9c7903dd1a9551c50d4a81ae5dd972a20c05e032
Danang Kusuma <danang.kusumayudha@mitrais.com>
2023-04-25 14:44:28 +0800

    Initial Commit

---
+++ Controllers/GoogleController.cs
@@ -0,0 +1,260 @@
+﻿using BuzCalendarV2.Models;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Calendar.v3.Data;
+using System.Globalization;
+using Calendar = Google.Apis.Calendar.v3.Data.Calendar;
+
+namespace BuzCalendarV2.Controllers
+{
+    public class GoogleController : Controller
+    {
+        private readonly IConfiguration _config;
+        public GoogleController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult GoogleCallBack(string code)
+        {
+            return Ok(code);
+        }
+
+        public IActionResult BuildConsentURL()
+        {
+            string[] scopes = { "https://www.googleapis.com/auth/calendar" };
+            string url = GoogleHelper.BuildConsentURL(
+                _config.GetValue<string>("ClientID"),
+                scopes,
+                "code",
+                "offline",
+                "true",
+                "consent",
+                 _config.GetValue<string>("Callbackurl")
+                );
+            return Ok(url);
+        }
+
+        public async Task<IActionResult> GetTokenAsync(string code)
+        {
+            GoogleAuthResponse tokenResponse = await GoogleHelper.ExchangeAuthorizationCode(code,
+                _config.GetValue<string>("ClientID"),
+                _config.GetValue<string>("Secret"),
+                _config.GetValue<string>("Callbackurl")
+                );
+            return Ok(tokenResponse);
+        }
+
+        public async Task<IActionResult> RefreshTokenAsync(string token)
+        {
+            GoogleAuthResponse tokenResponse = await GoogleHelper.ExchangeRefreshToken(token,
+               _config.GetValue<string>("ClientID"),
+               _config.GetValue<string>("Secret")
+                );
+            return Ok(tokenResponse);
+        }
+
+
+        public async Task<IActionResult> GetCalendarList(string accessToken, string refreshToken)
+        {
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _config.GetValue<string>("ClientID"),
+                    ClientSecret = _config.GetValue<string>("Secret")
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            }); ;
+
+            var credential = new UserCredential(flow, "user_id", new TokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            });
+
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            var events = await service.CalendarList.List().ExecuteAsync();
+
+            return Ok(events);
+        }
+
+        public async Task<IActionResult> GetCalendar(string accessToken, string refreshToken, string calendarID)
+        {
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _config.GetValue<string>("ClientID"),
+                    ClientSecret = _config.GetValue<string>("Secret")
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            });
+
+            var credential = new UserCredential(flow, "user_id", new TokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            });
+
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            var events = await service.CalendarList.Get(calendarID).ExecuteAsync();
+
+            return Ok(events);
+        }
+
+        public async Task<IActionResult> GetCalendarEvent(string accessToken, string refreshToken, string calendarID)
+        {
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _config.GetValue<string>("ClientID"),
+                    ClientSecret = _config.GetValue<string>("Secret")
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            });
+
+            var credential = new UserCredential(flow, "user_id", new TokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            });
+
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            var events = await service.Events.List(calendarID).ExecuteAsync();
+
+            return Ok(events);
+        }
+
+        public async Task<IActionResult> AddCalendar(string accessToken, string refreshToken, string calendarID)
+        {
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _config.GetValue<string>("ClientID"),
+                    ClientSecret = _config.GetValue<string>("Secret")
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            });
+
+            var credential = new UserCredential(flow, "user_id", new TokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            });
+
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            Calendar calendar = new Calendar();
+
+            calendar.Summary = "Test Buzz Calendar Summary";
+            calendar.TimeZone = "Australia/Melbourne";
+
+            var res = await service.Calendars.Insert(calendar).ExecuteAsync();
+
+            return Ok(res);
+
+        }
+
+        public async Task<IActionResult> AddCalendarEvent(string accessToken, string refreshToken, string calendarID)
+        {
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _config.GetValue<string>("ClientID"),
+                    ClientSecret = _config.GetValue<string>("Secret")
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            });
+
+            var credential = new UserCredential(flow, "user_id", new TokenResponse
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            });
+
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            Event calendarEvent = new Event();
+            calendarEvent.Summary = "Test Buzz Calendar Summary";
+            calendarEvent.Location = "Test Buzz Calendar Location";
+            calendarEvent.Description = "Test Buzz Calendar Description";
+
+            DateTime startDateTime = DateTime.Parse("2023-05-28T09:00:00-07:00");
+            EventDateTime start = new EventDateTime();
+            start.DateTime = startDateTime;
+            start.TimeZone = "Australia/Melbourne";
+            calendarEvent.Start = start;
+
+            DateTime endDateTime = DateTime.Parse("2023-05-28T09:00:00-08:00");
+            EventDateTime end = new EventDateTime();
+            end.DateTime = endDateTime;
+            end.TimeZone = "Australia/Melbourne";
+            calendarEvent.End = end;
+
+            List<string> recurrence = new List<string> { "RRULE:FREQ=DAILY;COUNT=2" };
+            calendarEvent.Recurrence = recurrence;
+
+            EventAttendee[] attendees = new EventAttendee[] {
+                new EventAttendee
+                {
+                    Email = "kusumayudha32@gmail.com"
+                }
+            };
+
+            calendarEvent.Attendees = attendees;
+
+            EventReminder[] reminderOverrides = new EventReminder[] {
+                new EventReminder
+                {
+                    Method = "email",
+                    Minutes = 24 * 60
+                },
+                new EventReminder
+                {
+                    Method = "popup",
+                    Minutes = 10
+                }
+            };
+
+            Event.RemindersData reminders = new Event.RemindersData
+            {
+                UseDefault = false,
+                Overrides = reminderOverrides
+            };
+
+            calendarEvent.Reminders = reminders;
+            var res = await service.Events.Insert(calendarEvent,calendarID).ExecuteAsync();
+
+            return Ok(res.HtmlLink);
+
+        }
+
+
+    }
+}
---
+++ Models/GoogleHelper.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace BuzCalendarV2.Models
+{
+    public class GoogleHelper
+    {
+        public static string BuildConsentURL(string clientId, string[] scope,string response_type,string access_type,string include_granted_scopes, string prompt, string redirectUri = null)
+        {
+            return $"https://accounts.google.com/o/oauth2/auth?client_id={clientId}&redirect_uri={redirectUri}&scope={string.Join(" ", scope)}&response_type=code&access_type={access_type}&include_granted_scopes={include_granted_scopes}&prompt={prompt}";
+        }
+
+        private static string BuildAuthorizationCodeRequest(string code, string clientId, string secret,
+            string redirectUri)
+        {
+            return
+                $"code={code}&client_id={clientId}&client_secret={secret}&redirect_uri={redirectUri}&grant_type=authorization_code";
+        }
+
+        private static string BuildRefreshAccessTokenRequest(string refreshToken, string clientId, string secret)
+        {
+            return
+                $"client_id={clientId}&client_secret={secret}&refresh_token={refreshToken}&grant_type=refresh_token";
+        }
+
+
+        private static async Task<GoogleAuthResponse> PostMessage(string postData)
+        {
+            GoogleAuthResponse result;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://oauth2.googleapis.com/");
+            var request = new HttpRequestMessage(HttpMethod.Post, "token");
+            request.Content = new StringContent(postData, Encoding.UTF8, "application/x-www-form-urlencoded");
+            var response = await client.SendAsync(request);
+            using (var content = response.Content)
+            {
+                var json = content.ReadAsStringAsync().Result;
+                result = JsonSerializer.Deserialize<GoogleAuthResponse>(json);
+            }
+            return result;
+        }
+
+
+        public static async Task<GoogleAuthResponse> ExchangeAuthorizationCode(string code, string clientId, string secret,
+            string redirectUri = null)
+        {
+            var result = new GoogleAuthResponse();
+
+            var postData = BuildAuthorizationCodeRequest(code, clientId, secret, redirectUri);
+
+            return await PostMessage(postData);
+        }
+
+        public static async Task<GoogleAuthResponse> ExchangeRefreshToken(string refreshToken, string clientId, string secret)
+        {
+            var postData = BuildRefreshAccessTokenRequest(refreshToken, clientId, secret);
+
+            return await PostMessage(postData);
+        }
+    }
+}
